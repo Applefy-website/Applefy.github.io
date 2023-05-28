d76183a39dad316474b8f076f782871d2e759830
Gangstaaaaa <85770558+Gangstaaaaa@users.noreply.github.com>
2023-05-28 18:47:52 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,52 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Inception - Home</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll active" href="#home">Home</a>
+        <li class="nav-item">
+          <a class="nav-link js-scroll" href="server.php">Information</a>
+        <li class="nav-item">
+          <a class="nav-link js-scroll" href="#about">Rules</a>
+        <li class="nav-item">
+          <a class="nav-link js-scroll" href="#work">Pictures</a>
+        <li class="nav-item">
+          <a class="nav-link js-scroll active" href="whitelist.php">Whitelist</a>
+        <li class="nav-item">
+          <a href="https://discord.gg/InceptionRP" class="nav-link js-scroll">Discord</a>
+        <li class="nav-item">
+          <a href="fivem://connect/o5vxvv" class="nav-link js-scroll">Connect</a>
+        <li class="nav-item">
+          <a href="https://rp-inception.tebex.io/" class="nav-link js-scroll">Tebex</a>
+        <li class="nav-item">
+          <a href="login.php" class="nav-link js-scroll">Login</a>
+        <li class="nav-item">
+          <a href="signup.php" class="nav-link js-scroll">Signup</a>
+        </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
